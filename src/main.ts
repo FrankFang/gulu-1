09867ac75d36814d9d4aa02e7e6457b09243e49c
Yichen <2283815345@qq.com>
2021-09-18 15:17:27 +0800

    topnav样式更新

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
-import {createWebHashHistory,createRouter} from 'vue-router'
+import './index.scss'
+import { createWebHashHistory, createRouter } from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
-
 const history = createWebHashHistory()
 const router = createRouter({
     history: history,
     routes: [
-        { path: '/', component: Home},
-        { path: '/doc', component:Doc},
+        { path: '/', component: Home },
+        { path: '/doc', component: Doc }
     ]
 })
-
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
