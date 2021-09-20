a8f295cc2c01da7686a8fd77c2d929137798f565
Yichen <2283815345@qq.com>
2021-09-20 23:16:36 +0800

    基础样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,22 +1,66 @@
 <template>
-  <button class="gulu-button"
-    :class="{[`theme-${theme}`]: theme}"
-  >
+    <button class="gulu-button" :class="classes">
     <slot />
   </button>
 </template>
 <script lang="ts">
+import { computed } from "vue";
 export default {
   props: {
     theme: {
       type: String,
-      default: 'button'
+      default: "button",
     },
+     size: {
+      type: String,
+      default: "normal",
+    },
+  },
+  setup(props) {
+    const { theme, size } = props;
+    const classes = computed(() => {
+      return {
+        [`gulu-theme-${theme}`]: theme,
+        [`gulu-size-${size}`]: size,
+      };
+    });
+    return { classes };
   },
 };
 </script>
 <style lang="scss" scoped>
-div {
-  border: 1px solid red;
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.gulu-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
 }
 </style> 
