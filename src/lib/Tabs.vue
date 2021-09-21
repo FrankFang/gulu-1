76e7473eedcf754c93e8366ab3512f36c6fd2ff6
Yichen <2283815345@qq.com>
2021-09-21 14:11:37 +0800

    采用尤雨溪的建议

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -5,7 +5,7 @@
     <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
   </div>
   <div class="gulu-tabs-content">
-    <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
+  <component :is="current" :key="current.props.title" />
   
   </div>
 </div>
@@ -16,7 +16,7 @@ import Tab from './Tab.vue'
 import {
   computed,
   ref,
-  watchEffect，
+  watchEffect,
     onMounted
   } from 'vue'
 export default {
@@ -29,6 +29,7 @@ export default {
     const selectedItem = ref < HTMLDivElement > (null)
     const indicator = ref < HTMLDivElement > (null)
     const container = ref < HTMLDivElement > (null)
+
     onMounted(() => {
       watchEffect(() => {
         const {
@@ -52,6 +53,9 @@ export default {
         throw new Error('Tabs 子标签必须是 Tab')
       }
     })
+    const current = computed(() => {
+      return defaults.find(tag => tag.props.title === props.selected)
+    })
 
     const titles = defaults.map((tag) => {
       return tag.props.title
@@ -60,6 +64,7 @@ export default {
       context.emit('update:selected', title)
     }
     return {
+      current,
       defaults,
       titles,
       select,
@@ -68,6 +73,7 @@ export default {
       container
     }
   }
+}
 
 </script>
 <style lang="scss">
@@ -85,6 +91,7 @@ $border-color: #d9d9d9;
       padding: 8px 0;
       margin: 0 16px;
       cursor: pointer;
+
     
     &:first-child {
         margin-left: 0;
@@ -108,12 +115,6 @@ $border-color: #d9d9d9;
 
   &-content {
     padding: 8px 0;
-        &-item {
-      display: none;
-      &.selected {
-        display: block;
-      }
-    }
   }
 }
 </style>
