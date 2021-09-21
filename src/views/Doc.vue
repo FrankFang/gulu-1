8b5fdf03963dfe6ece5d4455210a98423121b5fc
Yichen <2283815345@qq.com>
2021-09-21 16:14:05 +0800

    更新切换按钮的样式

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,49 +1,60 @@
 <template>
-  <div class="layout">
-    <Topnav class="nav" />
-    <div class="content">
-      <aside v-if="menuVisible">
-        <h2>组件列表</h2>
-        <ol>
-          <li>
-             <router-link to="/doc/switch">Switch 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/button">Button 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/dialog">Dialog 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/tabs">Tabs 组件</router-link>
-          </li>
-        </ol>
-      </aside>
-        <main>
-        <router-view />
-      </main>
-    </div>
+<div class="layout">
+  <Topnav toggleMenuButtonVisible class="nav" />
+  <div class="content">
+    <aside v-if="menuVisible">
+      <h2>组件列表</h2>
+      <ol>
+        <li>
+          <router-link to="/doc/switch">Switch 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/button">Button 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/dialog">Dialog 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/tabs">Tabs 组件</router-link>
+        </li>
+      </ol>
+    </aside>
+    <main>
+      <router-view />
+    </main>
+  </div>
   </div>
 </template>
+
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
-import { inject, Ref } from "vue";
+import {
+  inject,
+  Ref
+} from "vue";
 export default {
-  components: { Topnav },
+  components: { 
+      Topnav 
+      },
   setup() {
-    const menuVisible = inject<Ref<boolean>>("menuVisible"); // get
-    return { menuVisible };
+    const menuVisible = inject < Ref < boolean >> ("menuVisible"); // get
+    return {
+      menuVisible
+    };
   },
 };
 </script>
+
 <style lang="scss" scoped>
 .layout {
   display: flex;
   flex-direction: column;
   height: 100vh;
+
   > .nav {
     flex-shrink: 0;
   }
+
   > .content {
     flex-grow: 1;
     padding-top: 60px;
@@ -53,17 +64,21 @@ export default {
     }
   }
 }
+
 .content {
   display: flex;
+
   > aside {
     flex-shrink: 0;
   }
+
   > main {
     flex-grow: 1;
     padding: 16px;
     background: white;
   }
 }
+
 aside {
   background: lightblue;
   width: 150px;
@@ -73,15 +88,18 @@ aside {
   left: 0;
   padding-top: 70px;
   height: 100%;
+
   > h2 {
     margin-bottom: 4px;
   }
+
   > ol {
     > li {
       padding: 4px 0;
     }
   }
 }
+
 main {
   overflow: auto;
 }
