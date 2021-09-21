d5f15198edf733fb36f32cfe967e53436e885eb9
Yichen <2283815345@qq.com>
2021-09-21 14:06:58 +0800

    修复 selectedItem.value is null

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,20 +1,23 @@
 <template>
 <div class="gulu-tabs">
   <div class="gulu-tabs-nav" ref="container">
-    <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (el) navItems[index] = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
-     <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+    <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+    <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
   </div>
   <div class="gulu-tabs-content">
     <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
+  
   </div>
 </div>
 </template>
+
 <script lang="ts">
 import Tab from './Tab.vue'
 import {
   computed,
   ref,
-  watchEffect
+  watchEffect，
+    onMounted
   } from 'vue'
 export default {
   props: {
@@ -26,20 +29,22 @@ export default {
     const selectedItem = ref < HTMLDivElement > (null)
     const indicator = ref < HTMLDivElement > (null)
     const container = ref < HTMLDivElement > (null)
-    watchEffect ( () => {
-      
-      const {
-        width
-      } = selectedItem.value.getBoundingClientRect()
-      indicator.value.style.width = width + 'px'
-      const {
-        left: left1
-      } = container.value.getBoundingClientRect()
-      const {
-        left: left2
-      } = selectedItem.value.getBoundingClientRect()
-      const left = left2 - left1
-      indicator.value.style.left = left + 'px'
+    onMounted(() => {
+      watchEffect(() => {
+        const {
+          width
+        } = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {
+          left: left1
+        } = container.value.getBoundingClientRect()
+        const {
+          left: left2
+        } = selectedItem.value.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      })
+    })
 
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
